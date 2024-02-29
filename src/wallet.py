33b8da6052a12d740f2317be024cf518d987ca2c
吉村直人 <ahowateto123@gmail.com>
2024-02-29 23:32:52 +0900

    ブロックチェーンアドレスの生成

--- src/wallet.py
+++ src/wallet.py
@@ -1,9 +1,15 @@
+import base58
+import binascii
+import codecs
+import hashlib
+
 from ecdsa import SigningKey, NIST256p
 
 class Wallet:
     def __init__(self):
         self._private_key = SigningKey.generate(curve=NIST256p)
         self._public_key = self._private_key.get_verifying_key()
+        self._blockchain_address = self.generate_blockchain_address()
     
     @property
     def private_key(self):
@@ -12,15 +18,42 @@ class Wallet:
     @property
     def public_key(self):
         return self._public_key.to_string().hex()
+    
+    @property
+    def blockchain_address(self):
+        return self._blockchain_address
 
-    def sign(self, data):
-        return self._private_key.sign(data)
-
-    def verify(self, signature, data):
-        return self._public_key.verify(signature, data)
+    def generate_blockchain_address(self):
+        # 1. SHA-256 for the public key
+        public_key_bytes = self._public_key.to_string()
+        sha256_bpk = hashlib.sha256(public_key_bytes)
+        sha256_bpk_digest = sha256_bpk.digest()
+        # 2. RIPEMD-160 for the SHA-256
+        ripemed160_bpk = hashlib.new('ripemd160')
+        ripemed160_bpk.update(sha256_bpk_digest)
+        ripemed160_bpk_digest = ripemed160_bpk.digest()
+        ripemed160_bpk_hex = codecs.encode(ripemed160_bpk_digest, 'hex')
+        # 3. Adding network byte
+        network_byte = b'00'
+        network_bitcoin_public_key = network_byte + ripemed160_bpk_hex
+        network_bitcoin_public_key_bytes = codecs.decode(network_bitcoin_public_key, 'hex')
+        # 4. Double SHA-256
+        sha256_bpk = hashlib.sha256(network_bitcoin_public_key_bytes)
+        sha256_bpk_digest = sha256_bpk.digest()
+        sha256_2_nbpk = hashlib.sha256(sha256_bpk_digest)
+        sha256_2_nbpk_digest = sha256_2_nbpk.digest()
+        sha256_hex = codecs.encode(sha256_2_nbpk_digest, 'hex')
+        # 5. Get checksum
+        checksum = sha256_hex[:8]
+        # 6. Concatenate public key and checksum
+        address_hex = (network_bitcoin_public_key + checksum).decode('utf-8')
+        # 7. Base58 encoding
+        blockchain_address = base58.b58encode(binascii.unhexlify(address_hex)).decode('utf-8')
+        return blockchain_address
 
 
 if __name__ == '__main__':
     wallet = Wallet()
     print(wallet.private_key)
     print(wallet.public_key)
+    print(wallet.blockchain_address)
