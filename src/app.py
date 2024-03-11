c1a607b01d8d309dbcb5a6dc8438f1e2eae0a37c
吉村直人 <ahowateto123@gmail.com>
2024-03-12 01:41:06 +0900

    チェーン取得API

--- src/app.py
+++ src/app.py
@@ -5,6 +5,29 @@ import wallet
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+app.config['port'] = 5000
+
+cache = {}
+def get_blockchain():
+    cached_blockchain = cache.get('blockchain')
+    if not cached_blockchain:
+        miners_wallet = wallet.Wallet()
+        cache['blockchain'] = blockchain.BlockChain(
+            blockchain_address=miners_wallet.blockchain_address,
+            port=app.config['port']
+        )
+        app.logger.warning({
+            'private_key': miners_wallet.private_key,
+            'public_key': miners_wallet.public_key,
+            'blockchain_address': miners_wallet.blockchain_address
+        })
+    return cache['blockchain']
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    block_chain = get_blockchain()
+    response = {
+        'chain': block_chain.chain,
+        'length': len(block_chain.chain)
+    }
+    return jsonify(response), 200
